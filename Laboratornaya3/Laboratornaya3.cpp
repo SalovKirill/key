1bc13b083a05f5ab3f2d67343fe2f55ba34a9641
SalovKirill <32333692+SalovKirill@users.noreply.github.com>
2017-12-18 21:10:20 +0300

    Laboratornaya#3
    Laboratornaya#3

---
+++ Laboratornaya3/Laboratornaya3.cpp
@@ -0,0 +1,80 @@
+// Laboratornaya3.cpp: определяет точку входа для консольного приложения.
+/*
+Салов Кирилл Андреевич
+Лабораторная работа №3
+Вариант №2
+Задание: Написать программу, которая во вводимом с клавиатуры тексте выберет слова,
+начинающиеся с гласной буквы, и выведет их на экран.
+*/
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <conio.h>
+#include <malloc.h>
+#include <ctype.h>
+#include <string.h>
+
+int WordLength = 0;
+
+bool IsVowel(char c)
+{
+	if (c == 'A' || c == 'a'
+		|| c == 'E' || c == 'e'
+		|| c == 'O' || c == 'o'
+		|| c == 'U' || c == 'u'
+		|| c == 'I' || c == 'i'
+		|| c == 'Y' || c == 'y')
+		return 1;
+	else return 0;
+
+}
+
+void Add(char c, char* word)
+{
+	if (WordLength)
+	{
+		if (word[WordLength - 1] == 10 && c == 10)
+			return;
+	}
+	word = (char*)realloc(word, (WordLength + 2) * sizeof(char));
+	word[WordLength] = c;
+	word[WordLength + 1] = '\0';
+	WordLength++;
+}
+
+int main()
+{
+	char *Word = (char*)calloc(1, sizeof(char));
+	char c;
+	int state = 0;
+	do
+	{
+		c = getchar();
+		if (isspace(c) || c == 26)
+		{
+			if (state == 1)
+			{
+				Add('\n', Word);
+			}
+			state = 0;
+		}
+		else if (state == 0)
+		{
+			if (IsVowel(c))
+			{
+				state = 1;
+				Add(c, Word);
+			}
+			else state = 2;
+		}
+		else if (state == 1)
+		{
+			Add(c, Word);
+		}
+	}
+	while (c != 26 && c != EOF);
+	printf("%s", Word);
+	_getch();
+    return 0;
+}
+ 
